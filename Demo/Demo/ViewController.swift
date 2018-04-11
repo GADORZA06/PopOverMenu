4dd5f4b66a13acf2914234fbac130e90548413b6
EarlSmartwave <37095419+EarlSmartwave@users.noreply.github.com>
2018-04-11 16:59:02 +0800

    Feature/andr 1689 mn (#2)
    * Allow use without storyboards
    
    * Implemented adding of images
    
    * Added customizing of corner radius
    
    * Changed Deployment Target
    
    * Implemented multiple selections
    
    * Allow customization of width and height
    
    * shorthand notation for assignment of enum values
    
    * Restructured if-else blocks for initializing/reusing cells
    
    * Refactored: reduced to property observer didSet
    
    * Forgot about these
    
    * Applied Pull Request Changes

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by user1 on 13/03/2018.
+//  Copyright © 2018 Smartwave. All rights reserved.
+//
+
+import UIKit
+import PopOverMenuComponent
+
+class ViewController: UIViewController {
+    let button = UIButton()
+    var selectedRows: Set<Int> = Set<Int>()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.setTitle("Press Me", for: UIControlState.normal)
+        button.setTitleColor(UIColor.black, for: UIControlState.normal)
+        button.frame = CGRect(x: 32, y: 32, width: 128, height: 64)
+        button.addTarget(self, action: #selector(buttonPressed), for: UIControlEvents.touchDown)
+        self.view.addSubview(button)
+    }
+
+    @objc func buttonPressed() {
+        print("Button Pressed!")
+        
+        let popup = PopOverViewController.instantiate(withSourceView: button)
+        
+        popup.setTitles(["Bob", "Ray", "Charlie", "Alpha", "Delta"])
+        popup.setDescriptions(["The Man", "The Technician", "The Mascot", "The Beta", "The Big D"])
+        popup.selectRows(self.selectedRows)
+        popup.setImageNames(["location", "location", "location", "location", "location"])
+        popup.setSelectedImageNames(["play", "play", "play", "play", "play"])
+        popup.setTitleColor(UIColor.blue)
+        popup.setSelectedTitleColor(UIColor.red)
+        popup.completionHandler = { [unowned self] (selectRow: Int) in
+            if self.selectedRows.contains(selectRow) {
+                self.selectedRows.remove(selectRow)
+            } else {
+                self.selectedRows.insert(selectRow)
+            }
+        }
+        
+        popup.cornerRadius = 4
+        
+        present(popup, animated: true, completion: nil)
+        popup.width = 400
+        popup.heightPerItem = 64
+    }
+}
+
+extension ViewController: UIPopoverPresentationControllerDelegate {
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+}
